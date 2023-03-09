cd16128ec821397f2abbb926134b827f57d66892
xietsunzao <lordshinjo22@gmail.com>
2023-03-10 03:29:05 +0700

    refactor(Controller): :recycle: Add method show for AccountController

--- app/Http/Controllers/Backend/AccountController.php
+++ app/Http/Controllers/Backend/AccountController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Account;
+use App\Http\Requests\AccountRequest;
 
 class AccountController extends Controller
 {
-    public function index() : \Illuminate\Http\JsonResponse
+    public function index()
     {
         $accounts = Account::all();
         return response()->json($accounts);
+    }
+
+    public function show(Account $account)
+    {
+        return response()->json($account);
     }
 }
