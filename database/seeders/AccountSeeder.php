74833bac8548e5f1b0f0545f997a86e6f58f6802
xietsunzao <lordshinjo22@gmail.com>
2023-03-09 19:48:21 +0700

    feat(Seeder): :seedling: Add AccountSeeder

---
+++ database/seeders/AccountSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AccountSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('accounts')->insert([
+            [
+                'name' => 'Customer1'
+            ],
+            [
+                'name' => 'Customer2'
+            ]
+        ]);
+    }
+}
