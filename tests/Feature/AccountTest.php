6d08ec0b37da00d35652766630c1464ceedbde32
xietsunzao <lordshinjo22@gmail.com>
2023-03-10 19:27:49 +0700

    test(Test): :white_check_mark: Add test get token , create data, update and delete for AccountTest

--- tests/Feature/AccountTest.php
+++ tests/Feature/AccountTest.php
@@ -8,15 +8,75 @@ use Tests\TestCase;
 
 class AccountTest extends TestCase
 {
+
+    public function getToken()
+    {
+        $response = $this->post('/api/login', [
+            'email' => 'admin@admin.com',
+            'password' => 'password',
+        ]);
+        $response->assertStatus(200);
+        $token = $response->json('access_token');
+        return $token;
+    }
+
     public function testAllAcount()
     {
-        $response = $this->get('/api/accounts');
+        $response = $this->get('/api/accounts', [
+            'Authorization' => 'Bearer ' . $this->getToken()
+        ]);
         $response->assertStatus(200);
     }
 
     public function testSingleAccount($id = 1)
     {
-        $response = $this->get('/api/accounts/' . $id);
+        $response = $this->get('/api/accounts/' . $id, [
+            'Authorization' => 'Bearer ' . $this->getToken()
+        ]);
         $response->assertStatus(200);
+    }
+
+    public function testCreateAccount()
+    {
+        $token = $this->getToken();
+        // parse token
+        $response = $this->post('/api/accounts', [
+            'account_name' => 'Customer 5',
+        ], [
+            'Authorization' => 'Bearer ' . $token,
+        ]);
+        $response->assertStatus(200);
+    }
+
+    public function testUpdateAccount($id = 3)
+    {
+        $token = $this->getToken();
+        // parse token
+
+        $response = $this->post('/api/accounts/' . $id, [
+            'account_name' => 'Customer 5',
+        ], [
+            'Authorization' => 'Bearer ' . $token,
+        ]);
+
+        if ($response->status() == 200) {
+            $response->assertStatus(200);
+        } else {
+            $response->assertStatus(404);
+        }
+    }
+
+    public function testDeleteAccount($id = 3)
+    {
+        $token = $this->getToken();
+        // parse token
+        $response = $this->delete('/api/accounts/' . $id, [
+            'Authorization' => 'Bearer ' . $token,
+        ]);
+        if ($response->status() == 200) {
+            $response->assertStatus(200);
+        } else {
+            $response->assertStatus(404);
+        }
     }
 }
