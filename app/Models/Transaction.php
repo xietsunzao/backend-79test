91de4ef2413c8dd13665d944fa89cec661ae61b9
xietsunzao <lordshinjo22@gmail.com>
2023-03-10 21:11:03 +0700

    feat(Model): :sparkles: Add Model Transaction

---
+++ app/Models/Transaction.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'account_id',
+        'transaction_date',
+        'description',
+        'transaction_type',
+        'amount',
+    ];
+}
