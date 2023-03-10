6c262ae6b929e1224c71599e48830785b72d56fd
xietsunzao <lordshinjo22@gmail.com>
2023-03-10 19:50:22 +0700

    feat(Migration): :card_file_box: Add migration create table transactons

---
+++ database/migrations/2023_03_10_123727_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('account_id');
+            $table->dateTime('transaction_date');
+            $table->string('description');
+            $table->string('transaction_type');
+            $table->integer('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
